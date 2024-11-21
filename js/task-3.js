559660878e40f9a58a8931a8931d03008842e27e
Marthen100 <159732913+Marthen100@users.noreply.github.com>
2024-11-21 06:23:03 +0100

    Delete js/task-3.js

--- js/task-3.js
+++
@@ -1,6 +0,0 @@
-const textInput = document.querySelector("input#name-input");
-const output = document.querySelector("span#name-output");
-
-textInput.addEventListener("input", (event) => {
-  output.textContent = event.currentTarget.value.trim();
-});
