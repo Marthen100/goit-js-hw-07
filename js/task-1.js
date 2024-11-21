67df67afec2d85dce71fafaa32dcd1224d0267fa
Marthen100 <159732913+Marthen100@users.noreply.github.com>
2024-11-21 06:22:47 +0100

    Delete js/task-1.js

--- js/task-1.js
+++
@@ -1,8 +0,0 @@
-const list = document.querySelectorAll("#categories .item");
-
-console.log(`Number of Categories: ${list.length}`);
-
-list.forEach((element) => {
-  console.log(`Category: ${element.querySelector("h2").textContent}`);
-  console.log(`Elements: ${element.querySelectorAll("li").length}`);
-});
